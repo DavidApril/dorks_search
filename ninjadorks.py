f7bacaa23bc30a16a7f49cff74270844465426a1
David <davidagamez04@gmail.com>
2024-12-19 21:39:39 -0500

    feat: enhance environment setup with command-line arguments for search configuration

--- ninjadorks.py
+++ ninjadorks.py
@@ -1,14 +1,46 @@
-import requests
-from dotenv import load_dotenv
-import os
+from dotenv import load_dotenv, set_key
 from google_search import GoogleSearch
+import argparse
+import os
+import sys
 
-load_dotenv()
+def env_setup():
+    """
+    Configure the API key and search engine ID
+    """
+    api_key = input('Enter your Google API key: ')
+    search_engine_id = input('Enter your Google Search Engine ID: ')
+    set_key('.env', 'GOOGLE_API_KEY', api_key)
+    set_key('.env', 'SEARCH_ENGINE_ID', search_engine_id)
 
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-SEARCH_ENGINE_ID = os.getenv('SEARCH_ENGINE_ID')
+def main(args):
 
-query = 'filetype:sql "MySQL dump" (pass|password|passwd|pwd)'
+    env_exists = os.path.exists('.env')
+    if not env_exists or args.configure:
+        env_setup()
+        print('API key and search engine ID configured successfully.')
+        sys.exit(1)
+        
+    load_dotenv()
 
-gsearch = GoogleSearch(GOOGLE_API_KEY, SEARCH_ENGINE_ID)
-results = gsearch.search(query, 1, 1, 'lang_en')
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    SEARCH_ENGINE_ID = os.getenv('SEARCH_ENGINE_ID')
+    
+    if not args.query:
+        print('Please provide a query to search for. -q "query"')
+        sys.exit(1)
+ 
+    gsearch = GoogleSearch(GOOGLE_API_KEY, SEARCH_ENGINE_ID)
+    results = gsearch.search(args.query, args.start_page, args.pages, args.lang)
+
+    gsearch.print_results(results)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ninja Dorks')
+    parser.add_argument('-q', '--query', type=str, help='The dork to search for\ne.g. "filetype:sql "MySQL dump" (pass|password|passwd|pwd)"')
+    parser.add_argument('-s', '--start-page', type=int, help='The page to start searching from')
+    parser.add_argument('-p', '--pages', type=int, help='The number of pages to search')
+    parser.add_argument('-l', '--lang', type=str, help='The language to search')
+    parser.add_argument('-c', '--configure', action='store_true', help='Configure the API key and search engine ID')
+    args = parser.parse_args()
+    main(args)
